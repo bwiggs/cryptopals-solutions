6374d56742ebb9dfc09cc0789e10f63f2154da9a
Brian Wigginton <brianwigginton@gmail.com>
2017-10-12 23:04:42 -0500

    adds comments

--- set1/7-aes-in-ecb-mode/main.go
+++ set1/7-aes-in-ecb-mode/main.go
@@ -5,8 +5,6 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io/ioutil"
-
-	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
@@ -20,9 +18,10 @@ func main() {
 		panic(err)
 	}
 
-	numEntries := len(ciphertext) / aes.BlockSize
-	spew.Dump(numEntries)
+	// GoLang doesn't ship with an AES ECB cipher since it's insecure.
+	// see: https://github.com/golang/go/issues/5597
 
+	numEntries := len(ciphertext) / aes.BlockSize
 	str := ""
 	for i := 0; i < numEntries; i++ {
 		dst := make([]byte, aes.BlockSize)
